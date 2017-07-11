89d42459074a192cc276ad38f9ea2df95194c94d
royye62 <royye62@gmail.com>
2017-07-11 19:49:30 +0800

    my update

--- tour/concurrency/channels.go
+++ tour/concurrency/channels.go
@@ -2,6 +2,7 @@ package main
 
 import "fmt"
 
+// Channels are the pipes that connect concurrent goroutines
 func sum(s []int, c chan int) {
     sum := 0
     for _, v := range s {
@@ -17,6 +18,12 @@ func main() {
     go sum(s[:len(s)/2], c)
     go sum(s[len(s)/2:], c)
     x, y := <-c, <-c // receive from c
-
     fmt.Println(x, y, x+y)
+
+    messages := make(chan string)
+    go func() { messages <- "ping" }()
+    // By default sends and receives block until both the sender and receiver are ready. 
+    // This property allowed us to wait at the end of our program for the "ping" message without having to use any other synchronization.
+    msg := <-messages
+    fmt.Println(msg)
 }
--- tour/concurrency/select.go
+++ tour/concurrency/select.go
@@ -1,30 +1,39 @@
+// Go's _select_ lets you wait on multiple channel
+// operations. Combining goroutines and channels with
+// select is a powerful feature of Go.
+
 package main
 
-import "fmt"
 import "time"
-
-func fibonacci(c, quit chan int) {
-    x, y := 0, 1
-    for {
-        select {
-            case c <- x:
-                x, y = y, x+y
-            case <-quit:
-                fmt.Println("quit")
-                return
-        }
-   }
-}
+import "fmt"
 
 func main() {
-    c := make(chan int)
-    quit := make(chan int)
+
+    // For our example we'll select across two channels.
+    c1 := make(chan string)
+    c2 := make(chan string)
+
+    // Each channel will receive a value after some amount
+    // of time, to simulate e.g. blocking RPC operations
+    // executing in concurrent goroutines.
     go func() {
-        for i := 0; i < 10; i++ {
-            fmt.Println(<-c)
-        }
-        time.Sleep(1000 * time.Millisecond)
-        quit <- 0
+        time.Sleep(time.Second * 1)
+        c1 <- "one"
     }()
-    fibonacci(c, quit)
+    go func() {
+        time.Sleep(time.Second * 2)
+        c2 <- "two"
+    }()
+
+    // We'll use `select` to await both of these values
+    // simultaneously, printing each one as it arrives.
+    for i := 0; i < 2; i++ {
+        select {
+        case msg1 := <-c1:
+            fmt.Println("received", msg1)
+        case msg2 := <-c2:
+            fmt.Println("received", msg2)
+        }
+    }
 }
+
